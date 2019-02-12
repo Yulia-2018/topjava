8392c75506b7e2747cedc19a4c53f24360f96ed2
VESTADOM\MitinaUV <yulmit@mail.ru>
2019-02-12 11:31:35 +0700

    HW01: Editing index.html and MealsUtil

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -1,21 +1,15 @@
 package ru.javawebinar.topjava.util;
 
-import ru.javawebinar.topjava.MealsTestData;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MealsUtil {
-    public static void main(String[] args) {
-        List<Meal> meals = Arrays.asList(MealsTestData.MEAL_1, MealsTestData.MEAL_2, MealsTestData.MEAL_3, MealsTestData.MEAL_4, MealsTestData.MEAL_5, MealsTestData.MEAL_6);
-        List<MealTo> mealsWithExcess = getFilteredWithExcess(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        mealsWithExcess.forEach(System.out::println);
-    }
-
     public static List<MealTo> getFilteredWithExcess(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
                 .collect(
